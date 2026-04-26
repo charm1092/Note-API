ee95fbb0050462a05d6d6cf821435a882e434851
Roman Chamara <romacamara6@gmail.com>
2026-04-26 19:17:33 +0300

    added db

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"note_api/db/connection"
+	"note_api/db/tables"
 	"note_api/http"
 	"note_api/note"
 )
 
+// todo: сделать возможным удаление версий заметки
+// решить проблему: 
+/*
+	1. когда меняю и название и содержимое заметки, то в таблицу note_versions летит начальная версия заметки И версия заметки с измененным только названием, но прежним содержимым
+	2. сделать индитификатором не название заметки, а сделать прям айди, чтобы было удобней получать доступ ко всему и различать, так как в note_versions этого очень не хватает после добавления бд
+*/
+
 func main()  {
-	repo := note.NewRepository()
+	ctx := context.Background()
+	pool, err := connection.CreateConnection(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tables.CreateTables(ctx, pool)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repo := note.NewRepository(pool)
 	httpHandler := http.NewHTTPHandlers(repo)
 	httpServer := http.NewHTTPServer(httpHandler)
 
@@ -15,5 +37,3 @@ func main()  {
 		fmt.Println("failed to start http server:", err)
 	}
 }
-
-// todo: fix когда обновляю сразу название и содержимое заметки, отправляю запрос, пишется ошибка 404, при этом когда вывожу заметку по старому названию тоже 404, когда по новому, то она находится, но без изменения контента. то есть когда меняю все - меняется ток название , при этом выводя ошибку а не измененную заметку
