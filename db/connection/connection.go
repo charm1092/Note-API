5e7e4d61e4f53cb8326baeedc62bd8ca6a989daa
Roman Chamara <romacamara6@gmail.com>
2026-05-02 21:26:56 +0300

    feature/migrate

--- db/connection/connection.go
+++ db/connection/connection.go
@@ -2,13 +2,15 @@ package connection
 
 import (
 	"context"
+	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // "postgres://YourUserName:YourPassword@YourHostName:5432/YourDatabaseName"
+// postgres://postgres:1234@localhost:5432/postgres
 
 func CreateConnection(ctx context.Context) (*pgxpool.Pool, error) {
-	return pgxpool.New(
-		ctx, "postgres://postgres:1234@localhost:5432/postgres")
+	connection := os.Getenv("connection")
+	return pgxpool.New(ctx, connection)
 }
