ffc0fefd400f847d7e10267bd37930507179858a
Roman Chamara <romacamara6@gmail.com>
2026-05-03 10:40:06 +0300

    feature/pass

--- db/connection/connection.go
+++ db/connection/connection.go
@@ -8,9 +8,8 @@ import (
 )
 
 // "postgres://YourUserName:YourPassword@YourHostName:5432/YourDatabaseName"
-// postgres://postgres:1234@localhost:5432/postgres
 
 func CreateConnection(ctx context.Context) (*pgxpool.Pool, error) {
-	connection := os.Getenv("connection")
+	connection := os.Getenv("CONNECTION")
 	return pgxpool.New(ctx, connection)
 }
